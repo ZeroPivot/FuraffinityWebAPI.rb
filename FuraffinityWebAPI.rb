c4062c43bd03eb79c114a7a2b2f97d2d54551e99
Duke R Grable <zerothpivot@gmail.com>
2018-07-03 14:20:43 -0700

    Initial commit

---
+++ FuraffinityWebAPI.rb
@@ -0,0 +1,39 @@
+require 'mechanize'
+require 'rmagick'
+require 'twitter'
+
+# Fake API for furaffinity.net
+# By ZeroPivot - zerothpivot@gmail.com
+
+
+#Version 0.1 - 2018-07-03 - Initial functions, class, helper functions
+
+class FuraffinityWebAPI
+
+	# Initialize with a cookie file, usually taken from a web extension.
+	# Required.
+	def initialize(cookie_file:) 
+								 
+	end
+
+	def image_by_id(image_id)
+	end
+
+	def get_gallery(user_id_or_name)
+	end
+
+	def get_favorites(user)
+	end
+
+	def user_watchlist(user_id_or_name)
+	end
+
+	def user_profile(user_id_or_name)
+	end
+
+
+
+
+
+end
+
